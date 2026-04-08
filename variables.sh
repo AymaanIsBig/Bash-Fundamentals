f0a9dcdea1382e62a12a02b8173307429c83bf44
AymaanIsBig <jnarang@smail.pembinatrails.ca>
2026-04-08 10:36:00 -0500

    added troubleshooting notes

--- variables.sh
+++ variables.sh
@@ -9,3 +9,11 @@ age=16
 
 echo "My name is $name"
 echo "I am $age years old"
+
+# common mistake:
+# name = "Jashan" (spaces break it)
+
+# no spaces allowed
+# troubleshooting
+# if variable not printing:
+# check spelling and $
